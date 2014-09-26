f583d6f8b079d30ef5f3fb0714e1416affb958a6
ses10 <dses14@yahoo.com>
2014-09-26 18:57:38 -0400

    added a quantity field if seller bought and sold more than 1 of same item

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 
-
+// 
 var profitLabel =  document.getElementsByTagName("label")[0];
 
 // holds amount of money paid and sold
 var pricePaid;
 var priceSold;
 
+// number of items
+var quantity
+
 // buttons 
 var addPaid = document.getElementsByTagName("button")[0];
 var addSold = document.getElementsByTagName("button")[1];
-var calcProfit = document.getElementsByTagName("button")[2];
+var applyQuantity = document.getElementsByTagName("button")[2];
+var calcProfit = document.getElementsByTagName("button")[3];
 
 // Input boxes to input amount paid and sold
 var paidInput = document.getElementsByTagName("input")[0];
 var soldInput = document.getElementsByTagName("input")[1];
+var quantityInput = document.getElementsByTagName("input")[2];
 
 // bind event handlers to buttons
 addPaid.onclick = function setPricePaid() {pricePaid = parseFloat(paidInput.value);};
 addSold.onclick = function setPriceSold() {priceSold = parseFloat(soldInput.value);};
-calcProfit.onclick = function calculateProfit() {profitLabel.innerHTML = priceSold - pricePaid;};
+applyQuantity.onclick = function setQuantity() {quantity = parseInt(quantityInput.value);};
+
+// Calculate profit 
+calcProfit.onclick = function calculateProfit() {
+	
+	var profit = priceSold - pricePaid;
+	
+	// if no input in Quantity field 
+	if(isNaN(quantity))
+	{
+	 profitLabel.innerHTML = profit;
+	}
+	else 
+	profitLabel.innerHTML = profit * quantity;	
+};
 
